14a4066f9eeccd06b2272396578a2159783a8efc
p34-pac <peterpaultolulope@gmail.com>
2023-12-24 22:37:26 +0100

    worked on cart and some additional homepage section

--- assets/js/mob-men-toggle.js
+++ assets/js/mob-men-toggle.js
@@ -1,15 +1,53 @@
-let menuBtn = document.querySelector('.mobile-menu-btn')
 
-menuBtn.addEventListener('click', (e)=>{
+
+// menuBtn.addEventListener('click', (e)=>{
+//     let menu = menuBtn.getAttribute('data-toggle')
+//     if(document.querySelector(`.${menu}`).classList.contains("closed")){
+//         document.querySelector(`.${menu}`).classList.remove('closed')
+//         document.body.style.overflow = "hidden"
+//     }
+
+// })
+
+
+
+function close(closeBtn){
+    let close = closeBtn.getAttribute('data-toggle')
+    if(!document.querySelector(`.${close}`).classList.contains("closed")){
+        console.log(document.querySelector(`.${close}`))
+        document.querySelector(`.${close}`).classList.add('closed')
+        document.body.style.overflow = "auto"
+    }
+}
+function open(menuBtn){
     let menu = menuBtn.getAttribute('data-toggle')
-    document.querySelector(`.${menu}`).classList.remove('closed')
 
+    if(document.querySelector(`.${menu}`).classList.contains("closed")){
+        document.querySelector(`.${menu}`).classList.remove('closed')
+        document.body.style.overflow = "hidden"
+    }
+}
 
-})
+document.body.addEventListener('click', (e)=>{
+    let target = null
+    if(e.target.classList.contains("cover-rest")){
+        target = e.target
+        close(target)
 
-let closeBtn = document.querySelector('.close-slide')
+    }
+    if(e.target.classList.contains('close')){
+        target = e.target
+        close(target)
 
-closeBtn.addEventListener('click', (e)=>{
-    let menu = menuBtn.getAttribute('data-toggle')
-    document.querySelector(`.${menu}`).classList.add('closed')
+    }else if( e.target.parentElement.classList.contains('close')){
+        target = e.target.parentElement
+        close(target)
+    }else if(e.target.classList.contains('open')){
+        target = e.target
+        open(target)
+    }else if( e.target.parentElement.classList.contains('open')){
+        target = e.target.parentElement
+        open(target)
+    }
+
 })
